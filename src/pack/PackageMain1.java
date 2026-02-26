5a59dd14d0ec112cc9164bd7b6191b09f0a9eddc
cheol1203 <cheolwon36@naver.com>
2026-02-27 02:16:07 +0900

    package

---
+++ src/pack/PackageMain1.java
@@ -0,0 +1,9 @@
+package pack;
+
+public class PackageMain1 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        pack.a.User user = new pack.a.User();
+        //다른 패키지에 있는 클래스를 사용하려면 경로를 포함해야함
+    }
+}
